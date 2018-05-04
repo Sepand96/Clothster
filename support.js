215fb80f2895ac4349190004a1523dceaccf1903
sepand seraj <sepandseradj@gmail.com>
2018-05-04 15:27:24 +0430

    chatbox created and animations added!

---
+++ support.js
@@ -0,0 +1,9 @@
+function openchatbox() {
+    document.getElementById("chat_button").style.display="none";
+    document.getElementById("chatbox").style.display="block";
+}
+function closechatbox(){
+    document.getElementById("chatbox").style.display='none';
+    document.getElementById("chat_button").style.display='flex';
+    
+}
