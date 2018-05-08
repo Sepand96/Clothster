f6c874f18a538f5b7d4d1deea9d76dc15ddb4404
sepand seraj <sepandseradj@gmail.com>
2018-05-08 16:28:24 +0430

    support functionality completed!

--- support.js
+++ support.js
@@ -34,10 +34,11 @@ function processsupportinfo() {
         if (this.status == 200) {
             jsonData = JSON.parse(this.responseText);
             document.getElementById("supportname").innerHTML = jsonData.support.first +" "+ jsonData.support.last;
-            document.getElementById("supportimg").src = jsonData.support.picture;
+            document.getElementById("supportimg").src = jsonData.support.picture;            
+            setInterval(receivemsg(), 500);
         }
         else {
-            window.alert("Error " + xmlhttp.statusText);
+            window.alert("Error " + this.statusText);
         }
     }
 }
@@ -45,24 +46,54 @@ function submitmsg(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("post", "http://51.15.59.130:46260/send", true);
     xmlhttp.setRequestHeader('Content-Type', 'application/json');
-    var msg = document.getElementById("chatmsg").value;
+    var msg = document.getElementById("chatinput").value;
+    document.getElementById("chatinput").value = null;
     xmlhttp.send("{\"message\":\"" + escape(msg) + "\"}");
-    var newmsgdiv = document.createElement("div");
-    var msgdiv = document.createElement("div");
-    var userphoto = document.createElement("img");
-    userphoto.id = "supportimg";
-    userphoto.src = "supportprofilepic.png"
-    newmsgdiv.appendChild(userphoto);
-    newmsgdiv.appendChild(msgdiv);
-    newmsgdiv.style.display = "flex";
-    msgdiv.innerHTML = msg;
-    var parent = document.getElementById("chatarea");
-    parent.appendChild(newmsgdiv);
+    createmessage(true, msg);
 }
 var msgs = [];
 function receivemsg(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("get", "http://51.15.59.130:46260/fetch", true);
     xmlhttp.send(null);
-    xmlhttp.onreadystatechange = processsupportinfo;
+    xmlhttp.onreadystatechange = function(){
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                if (this.responseText === null)
+                 return;
+                else
+                    var msg = JSON.parse(this.responseText);
+                    msgs.push(msg.responses[0].message);
+                    createmessage(false, msg.responses[0].message);
+            }
+            else {
+                window.alert("Error " + this.statusText);
+            }
+        }
+    };
+}
+function createmessage(sender, msg){
+    var newmsgdiv = document.createElement("div");
+    var msgdiv = document.createElement("div");
+    var userphoto = document.createElement("img");
+    userphoto.id = "supportimg";
+    newmsgdiv.style.display = "flex";
+    newmsgdiv.style.flexdirection = "row-reverse";
+    newmsgdiv.style.borderTop = "1em";
+    msgdiv.innerHTML = msg;
+    msgdiv.className = "chatmessage";
+    if (sender === true)
+    {
+        userphoto.src = "supportprofilepic.png";
+        newmsgdiv.appendChild(userphoto);
+        newmsgdiv.appendChild(msgdiv);
+    }
+    else
+    {
+        userphoto.src = jsonData.support.picture;
+        newmsgdiv.appendChild(msgdiv);
+        newmsgdiv.appendChild(userphoto);
+    }
+    var parent = document.getElementById("chatarea");
+    parent.appendChild(newmsgdiv);
 }
